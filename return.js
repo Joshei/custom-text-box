34898ca59be4df8157d2f48a63f2f7997dca4ea7
josheir <joshuaeirm@gmail.com>
2023-09-20 15:33:48 -0400

    Return works.

--- return.js
+++ return.js
@@ -1,15 +1,13 @@
 class ReturnClass {
-  constructor () {
-    this.lastLine = 0
-    this.firstTimeThrough = true
-    this.holdsTextToMove = []
-  }
+  
   
   removeCharactersOnRightOnActiveLine() {
     for (let x = horizontalCursorPosition / 5; x < WIDTH; x++) {
+      //grid starts with, '-'stack overflow
       if (nestedArray[verticalCursorPosition/10][x] === '-') {
         return
       }
+      //Character represents space character
       nestedArray[verticalCursorPosition / 10][x] = 'A'
     }
   }
@@ -21,18 +19,19 @@ class ReturnClass {
     for (let y = (verticalCursorPosition / 10); y < HEIGHT - 1; y++) {
       for (let x = hp; x < WIDTH; x++) {
         if (tempArray[y][x] == '-') {
+          hp = 0
           break
         }
         let newValue = nestedArray[y][x] // sets 10 times
         console.log({ newValue })
-        tempArray[y + 1][(x - (horizontalCursorPosition/5))] = newValue
+        
+        
+        tempArray[y + 1][(x - hp)] = newValue
       }
     hp = 0;  
     }
     nestedArray = [...tempArray]
-    console.log("ta: " ,tempArray )
-    console.log("na: ", nestedArray)
-    this.removeCharactersOnRightOnActiveLine()
+     this.removeCharactersOnRightOnActiveLine()
     drawGrid()
     //advances vertical position to next row where cursor will be
     verticalCursorPosition = verticalCursorPosition + 10
