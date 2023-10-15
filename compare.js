5dba8f152b510c87a0babaa9346614715f61e1b6
josheir <joshuaeirm@gmail.com>
2023-10-15 09:57:37 -0400

    Commit

--- compare.js
+++ compare.js
@@ -1,5 +1,18 @@
+//10/5/23
+//Today worked on over the border text.  The code make the text to move erased on the original row.  First put a character on
+//end of row and than on string of characters
+
+
 class InsertClass {
-    z = 0 ;
+   
+    constructor(){
+      this.indexOfXForLineBeforeLastRowSpace = 0
+      this.StartedCapture = false
+      this.GotWordAllready = true
+      this.InsertWordToArray = false
+      this.IsFirstCharacterPos = 5
+      this.RemoveBeforeWidth = false
+    }
     makeTwoDimArrayWithInsertion() {
       //alert("1");
           //rows
@@ -56,6 +69,95 @@ class InsertClass {
         nextX
     }
   }
+
+  //put removeword at top, when called set flag to leave insert in removeword
+ removeWord(y){
+
+  console.log("A:", this.indexOfXForLineBeforeLastRowSpace)
+  console.log("here: ", verticalCursorPosition/10);
+  //if(nestedArray[0][9] == '-'){
+  //  alert("1");
+  //  return
+  //}
+
+  if(horizontalCursorPosition/5 === WIDTH-1){
+    //alert("2");
+    return
+  }
+
+  alert("here");
+  //alert("here");
+    
+    console.log("B:", y)
+    console.log("x:", x, "y: ", y);
+  let toMoveString = []
+  for(let x = this.indexOfXForLineBeforeLastRowSpace+1; x < WIDTH; x++){
+       let element = nestedArray[verticalCursorPosition/10][x]
+       toMoveString[x] = element
+       console.log("B: ", toMoveString)
+
+    }
+
+    console.log("C:", this.indexOfXForLineBeforeLastRowSpace+1)
+  for(let x = this.indexOfXForLineBeforeLastRowSpace+1; x < WIDTH; x++){
+  nestedArray[verticalCursorPosition/10][x] = 'Z'
+  }
+
+  //for(let x = this.indexOfXForLineBeforeLastRowSpace + 1 ; ){
+  //  nestedArray[y][x] = to
+  //}
+   
+    return(true)
+ 
+ }
+
+  findBeginningX(y, x, NewSpace){
+   
+    if(nestedArray[verticalCursorPosition/10][WIDTH-1] === '-'){
+
+      return
+    }
+
+    for (let x = 0; x < WIDTH-1; x++){
+      //get last space or, '-'
+      if (nestedArray[verticalCursorPosition/10][x] === '-' || nestedArray[verticalCursorPosition/10][x] === ' ')
+    
+      console.log("G:", this.indexOfXForLineBeforeLastRowSpace)
+      this.indexOfXForLineBeforeLastRowSpace = x;
+
+    }
+    return;
+
+    if(NewSpace === true){
+      this.indexOfXForLineBeforeLastRowSpace = x
+      this.IsFirstCharacterPos = x;
+      console.log("E:", x)
+      console.log("I: ",this.indexOfXForLineBeforeLastRowSpace )
+    }else{
+      //alert("here");
+      //this.indexOfXForLineBeforeLastRowSpace++;
+      if(this.indexOfXForLineBeforeLastRowSpace > WIDTH-1){
+        this.indexOfXForLineBeforeLastRowSpace = 0;
+      }
+      console.log("H: ",this.indexOfXForLineBeforeLastRowSpace )
+    }
+
+      console.log("WIDTH:", WIDTH, "A:", this.indexOfXForLineBeforeLastRowSpace)
+        if(x === WIDTH - 1)
+        {
+          if(nestedArray[verticalCursorPosition/10][x] == "-"){
+            return false;
+          }
+          //alert("width");
+           this.RemoveBeforeWidth = true;
+            return true;
+        }
+        return false;
+      }
+    //if there is a space than start capturing text, unless another space than start capture again
+    //get start position
+    //if the last position is filled than begin insert word on next line and push the line over to next row
+    //end insert on first space or null
     //inserts a character into array 
     insertCharacter() {
         //I need to stringfy and parse to copy the value
@@ -63,15 +165,17 @@ class InsertClass {
         //which means modify temp modifies the original
         //we want them to keep them seperate for the shift
       let tempArray = JSON.parse(JSON.stringify(nestedArray));
+      let GotWordAllready = false
       let maxX = WIDTH;
       let maxY = HEIGHT;
       let currentIndex = {
           y: verticalCursorPosition/10,
           x: horizontalCursorPosition/5
-
       }
+       let justWasASpace = true
        let currentColumn = verticalCursorPosition/10
        //only inserts to next column
+       this.RemoveBeforeWidth = false
         for(let y = 0; y <= currentColumn + 1; y++){
          // alert(currentColumn)
           for(let x = 0; x < maxX; x++){
@@ -79,14 +183,34 @@ class InsertClass {
               } else if( y <= currentIndex.y -1 ){
               }else {
                   let {nextY, nextX} = this.getNextTarget(y,x);
+                  
                   //console.log(x,y, nestedArray[x][y], "==>", nextX, nextY,  tempArray[nextX][nextY] );
                   let newValue = nestedArray[y][x];
                   tempArray[nextY][nextX] = newValue;
+                  
+                  
+                  if (newValue === '-'){
+                    justWasASpace = true;
+                  }else{
+                    //alert("else");
+                    justWasASpace = false
+                  }
+                  if(y === verticalCursorPosition/10){
+                    console.log("else2")
+                  let InsertWordToArray = this.findBeginningX(y,x, justWasASpace)
+                  }
               }
           }
       }
+    //this.IsFirstCharacterPos = ""
     nestedArray = [...tempArray];
     nestedArray[currentIndex.y][currentIndex.x] =  gKey;
+    //if(this.RemoveBeforeWidth === true){
+      console.log("!: ", nestedArray[verticalCursorPosition/10][WIDTH-1]);
+    if(nestedArray[verticalCursorPosition/10][WIDTH-1] !== '-'){
+      alert("rw");
+    this.removeWord(0)
+    }
       drawGrid()
       horizontalCursorPosition = horizontalCursorPosition + 5;
       if (((horizontalCursorPosition / 5) % (WIDTH)) === 0) {
