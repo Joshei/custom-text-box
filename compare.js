9502bfa6992e15f969a3b536a27b4af6a2f2a17a
josheir <joshuaeirm@gmail.com>
2023-10-26 21:21:56 -0400

    Nested array takes movestring.

--- compare.js
+++ compare.js
@@ -15,6 +15,7 @@ class InsertClass {
     this.skipThisFunction = false;
     this.skipSecondFunction = false;
     this.skipThirdFunction = false;
+    this.toMoveString = ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"];
   }
   makeTwoDimArrayWithInsertion() {
     //alert("1");
@@ -50,7 +51,7 @@ class InsertClass {
     } else if (x === WIDTH - 1) {
       nextX = 0;
       nextY = y + 1;
-      console.log("target: ", nestedArray);
+      
     } else {
       nextX = x + 1;
     }
@@ -83,7 +84,8 @@ class InsertClass {
     
     console.log("0:", nestedArray);
     console.log("C:", this.indexOfXForLineBeforeLastRowSpace);
-    for (let x = this.indexOfXForLineBeforeLastRowSpace+1; x <= WIDTH  ; x++) {
+    for (let x = this.indexOfXForLineBeforeLastRowSpace; x < WIDTH  ; x++) {
+      ///////////////
       nestedArray[verticalCursorPosition / 10][x] = "Y";
     }
     console.log("0:", nestedArray);
@@ -92,7 +94,7 @@ class InsertClass {
 
   //rewrite array after inserted movestring
   rebuildArrayWithMoveString(lengthOftoMoveString) {
-    return;
+    console.log("1Y:", nestedArray);
     let x1 = lengthOftoMoveString;
     for (let y = 0; y < HEIGHT - 1; y++) {
       for (let x = 0; x < WIDTH; x++) {
@@ -102,7 +104,8 @@ class InsertClass {
       //subtracts to zero
       x1 = 1;
     }
-    console.log("1:", nestedArray);
+    console.log("1X:", nestedArray);
+    console.log("1Z:", this.toMoveString);
   }
 
   
@@ -118,20 +121,21 @@ class InsertClass {
     }
     //alert("1");
     this.copyArray();
-    let toMoveString = ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"];
+    
     let x2 = 0;
 
     
     //build movestring
     console.log("A:", this.indexOfXForLineBeforeLastRowSpace);
-    for (let x = this.indexOfXForLineBeforeLastRowSpace + 1; x < WIDTH; x++) {
+    
+    for (let x = this.indexOfXForLineBeforeLastRowSpace ; x < WIDTH; x++) {
       let element = nestedArray[yValue][x];
-      toMoveString[x2] = element;
+      this.toMoveString[x2] = element;
       x2++;
     }
-    console.log("2: ", toMoveString);
+    console.log("2: ", this.toMoveString);
     console.log("3:", this.indexOfXForLineBeforeLastRowSpace);
-
+    console.log("AA:", nestedArray);
     this.fillMovedStringWithYs();
 
     //----------------------------------------------
@@ -139,26 +143,34 @@ class InsertClass {
     //let length = toMoveString.length;
 
     //find length of string  - FIX THIS SO ABORT IF MOVETEXT.LENGHT == 0, CHECK LAST CHARACTER, OK? and FIX ONLY LAST CHARACTER, WORKS THOUGH
-    let indexOfLength = -1;
+    let indexOfLength = 0;
     for(let i = 0; i < WIDTH ; i++){
 
-      if (toMoveString[i] === '-'){
-        return
+      if (this.toMoveString[i] === '-'){
+        break;
 
       }
       indexOfLength++;
     }
 
+
+
+    ////////////////////////////
+    let lengthIndex = indexOfLength;
+    let counter = 1
     
-    for (let x = 0; x < WIDTH; x++) {
+    //let moveStringIndex = 0
+    for (let x = 1 ; x <= lengthIndex ; x++) {
       //fill nestedarray with characters moved
       //if unmoveable than the content will be unchaged, '-'
-      if (toMoveString[x] != "-") {
-        let element = toMoveString[x];
-        nestedArray[verticalCursorPosition / 10][indexOfLength] = element;
-        length++
+      //if (this.toMoveString[lengthIndex] != "-") {
+        let element = this.toMoveString[lengthIndex - counter];
+        nestedArray[verticalCursorPosition / 10+1][x] = element;
+        //lengthIndex--;
+        counter++
+       // moveStringIndex++
       
-      }
+      //}
     }
     console.log("5:", nestedArray);
     this.rebuildArrayWithMoveString(length);
@@ -170,13 +182,15 @@ class InsertClass {
     //if (nestedArray[y][WIDTH - 1] === "-") {
     //  return;
     //}
-
-    for (let x = 0; x < WIDTH; x++) {
+    //index one before, so width-1
+    for (let x = 0; x < WIDTH-1; x++) {
       if (nestedArray[y][x] === "-") {
         //|| nestedArray[verticalCursorPosition/10][x] === 'Y')
 
+        //because the index is for a string that advances one space to right and goes to next row
         console.log("G:", this.indexOfXForLineBeforeLastRowSpace);
-        this.indexOfXForLineBeforeLastRowSpace = x;
+       
+        this.indexOfXForLineBeforeLastRowSpace = x + 1 ;
       }
     }
     console.log("X:", nestedArray);
@@ -221,11 +235,12 @@ class InsertClass {
 
           //console.log(x,y, nestedArray[x][y], "==>", nextX, nextY,  tempArray[nextX][nextY] );
           let newValue = nestedArray[y][x];
+          console.log("10A: ", tempArray);
           tempArray[nextY][nextX] = newValue;
         }
       }
     }
-
+   
     nestedArray = [...tempArray];
     //MODIFIED
     nestedArray[currentIndex.y][currentIndex.x] = gKey;
@@ -249,10 +264,11 @@ class InsertClass {
             return
           }
  
+  console.log("*:", nestedArray);
   //needs y
   this.findBeginningX(0, x);
   // once every y
-  console.log("*:", nestedArray);
+  
   this.removeWordAndWriteRowsAfter();
   //this.skipThisFunction = false;
   //this.skipSecondFunction = false;
